3dd6e50856e4b3dc663115dd6760024c4b150728
Tenzin Mahabir <tenzin.mahabir@mail.utoronto.ca>
2020-06-10 15:16:13 -0400

    Initial Commit

---
+++ iOS-DemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-DemoApp
+//
+//  Created by Tenzin Mahabir on 2020-06-10.
+//  Copyright © 2020 Tenzin Mahabir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
