8eca9b0ffcda30438ef753d056f4280ec7aad2b4
Tenzin Mahabir <tenzin.mahabir@mail.utoronto.ca>
2020-06-16 18:34:05 -0400

    Learned about Modifiers and Basic SwiftUI Elements

--- iOS-DemoApp/ContentView.swift
+++ iOS-DemoApp/ContentView.swift
@@ -10,7 +10,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            
+            Image("Breakfast").resizable()
+            VStack{
+                
+        
+                ZStack{
+                    
+                    Image("TopGradient")
+                    
+                    VStack(){
+                            
+                            Spacer()
+                            HStack(spacing: 165){
+                              
+                                Text("Hardcore\nAvo Toast").font(.title).fontWeight(.semibold).foregroundColor(Color.white).multilineTextAlignment(.leading)
+                                                                                               Image("Heart_health")
+                            }
+                           Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                           
+                    }
+
+                                
+                                
+                    }
+                    
+
+                ZStack{
+                    Image("BottomGradient")
+                    
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Text("Energize with this healthy and hearty breakfast").foregroundColor(.white)
+                        
+                        Spacer()
+                    }
+                
+                }
+            }
+        
+            
+        }
+        
+        /* VStack{
+            Image("pinkbuilding").resizable().cornerRadius(30).padding().shadow(radius: 10)
+            
+            Text("This building looks fabulous").fontWeight(.heavy).foregroundColor(.pink).font(.largeTitle).multilineTextAlignment(.center).shadow(color: .blue, radius: 3, x: 2, y: 2)
+            //Spacer()
+        }*/
+      
+        
+            
+    
     }
 }
 
