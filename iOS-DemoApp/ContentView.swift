6089a1aa8527ac6a4342746b6ea83d9cb6f6fc3a
Tenzin Mahabir <tmahabir10@gmail.com>
2021-01-08 18:52:28 -0500

    Created Basic Slots App

--- iOS-DemoApp/ContentView.swift
+++ iOS-DemoApp/ContentView.swift
@@ -10,85 +10,166 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isHearted = false
+    private var symbols = ["apple-1", "star", "cherry"]
+    @State private var numbers = [2, 1, 2]
+    
+    @State private var credits = 1000
+    private var betAmount = 5
+    
+    //@State private var isHearted = false
     //Make it private so it's only accessible by ContentView
-    @State private var numLikes = 999
+    //@State private var numLikes = 999
     
     var body: some View {
         
         ZStack {
             
-            Image("Breakfast").resizable().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            //Background
+            Rectangle().foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Rectangle().foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255)).rotationEffect(Angle(degrees: 45)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
             VStack{
+                Spacer()
                 
+                //Title
+                HStack{
+                    Image(systemName: "star.fill").foregroundColor(.yellow)
+                    
+                    Text("SwiftUI Slots").bold().foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill").foregroundColor(.yellow)
+                }.scaleEffect(2)
                 
-                ZStack{
+                Spacer()
+                
+                //Credits counter
+                Text("Credits: " + String(credits)).foregroundColor(.black).padding().background(Color.white.opacity(0.5)).cornerRadius(20)
+                
+                Spacer()
+                
+                //Cards
+                HStack {
+                    Spacer()
                     
-                    Image("TopGradient").padding(.bottom, 130.0)
+                    Image(symbols[numbers[0]]).resizable().aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5)).cornerRadius(20)
                     
+                    Image(symbols[numbers[1]]).resizable().aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5)).cornerRadius(20)
                     
-                    VStack(){
-                        
-                        Spacer()
-                        HStack(spacing: 120){
-                            
-                            Text("Hardcore\nAvo Toast").font(.title).fontWeight(.semibold).foregroundColor(Color.white).multilineTextAlignment(.leading)
-                            HStack(spacing: 10){
-                                Button(action: {
-                                    self.isHearted.toggle()
-                                    if isHearted {
-                                        numLikes += 1
-                                    } else {
-                                        numLikes -= 1
-                                    }
-                                }) {
-                                    if isHearted {
-                                        Image(systemName: "heart.fill")
-                                    } else {
-                                        Image(systemName: "heart")
-                                    }
-                                }.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                                
-                                //Image("Heart_health")
-                                
-                                Text(String(numLikes))
-                                    .font(.largeTitle)
-                                    .frame(width: 80.0)
-                            }
-                        }
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        
-                    }
+                    Image(symbols[numbers[2]]).resizable().aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5)).cornerRadius(20)
                     
-                    
-                    
+                    Spacer()
                 }
                 
+                Spacer()
                 
-                ZStack{
-                    Image("BottomGradient").padding(.top, 130.0)
+                //Button
+                Button(action: {
                     
-                    VStack {
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
+                    //Change the images
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count-1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count-1)
+                    
+                    //Check winnings
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
                         
-                        Text("Energize with this healthy and hearty breakfast").foregroundColor(.white)
-                        
-                        Spacer()
+                        self.credits += self.betAmount*10
+                    } else {
+                        self.credits -= self.betAmount
                     }
                     
+                }) {
+                    Text("Spin").bold().foregroundColor(.white).padding(.all, 10).padding(.horizontal, 30).background(Color.pink).cornerRadius(20)
                 }
+                
+                Spacer()
+                
             }
+            
+            
             
             
         }
         
+        //From Demo earlier in tutorial video
+        /* ZStack {
+         
+         Image("Breakfast").resizable().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+         VStack{
+         
+         
+         ZStack{
+         
+         Image("TopGradient").padding(.bottom, 130.0)
+         
+         
+         VStack(){
+         
+         Spacer()
+         HStack(spacing: 120){
+         
+         Text("Hardcore\nAvo Toast").font(.title).fontWeight(.semibold).foregroundColor(Color.white).multilineTextAlignment(.leading)
+         HStack(spacing: 10){
+         Button(action: {
+         self.isHearted.toggle()
+         if isHearted {
+         numLikes += 1
+         } else {
+         numLikes -= 1
+         }
+         }) {
+         if isHearted {
+         Image(systemName: "heart.fill")
+         } else {
+         Image(systemName: "heart")
+         }
+         }.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+         
+         //Image("Heart_health")
+         
+         Text(String(numLikes))
+         .font(.largeTitle)
+         .frame(width: 80.0)
+         }
+         }
+         Spacer()
+         Spacer()
+         Spacer()
+         Spacer()
+         Spacer()
+         
+         }
+         
+         
+         
+         }
+         
+         
+         ZStack{
+         Image("BottomGradient").padding(.top, 130.0)
+         
+         VStack {
+         Spacer()
+         Spacer()
+         Spacer()
+         Spacer()
+         
+         Text("Energize with this healthy and hearty breakfast").foregroundColor(.white)
+         
+         Spacer()
+         }
+         
+         }
+         }
+         
+         
+         } */
+        
+        //From Demo even earlier in the tutorial video
         /* VStack{
          Image("pinkbuilding").resizable().cornerRadius(30).padding().shadow(radius: 10)
          
